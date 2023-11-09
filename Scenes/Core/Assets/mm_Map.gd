1c8d82ddde06048cd7cb2365090a420a8f23fe23
JD Williams <jdwill239@gmail.com>
2023-11-09 13:01:27 -0500

    Add Map_Loading to Main Menu

---
+++ Scenes/Core/Assets/mm_Map.gd
@@ -0,0 +1,33 @@
+extends Panel
+
+
+signal Map_Selected(directory : String)
+@export var Title : String
+@export var Subtitle : String
+
+@export var Author : String
+@export var Mapper : String
+
+@export var Map_Image : String
+
+@export var map_directory : String
+
+func _ready() -> void:
+	$Title/Title.text = Title
+	$Title/Subtitle.text = Subtitle
+	
+	$Author/Author.text = Author
+	$Author/Mapper.text = Mapper
+	
+	$Directory.text = map_directory
+	
+	var image = Image.load_from_file(map_directory + "/" + Map_Image)
+	var texture = ImageTexture.create_from_image(image)
+	$TextureRect.texture = texture
+	
+	
+	
+
+
+func _on_selection_button_pressed() -> void:
+	Map_Selected.emit(map_directory)
--- Scenes/Core/main_menu.gd
+++ Scenes/Core/main_menu.gd
@@ -2,11 +2,33 @@ extends Control
 
 var config = ConfigFile.new()
 
-
+const MM_MAP = preload("res://Scenes/Core/Assets/mm_Map.tscn")
 
 
 func _ready() -> void:
+	
+	$ScrollContainer/VBoxContainer/Panel.queue_free()
+	
+	
 	var err = config.load("user://settings.cfg")
+	print(config.get_value("FileSystem", "InstallDir") + "/CustomWIPLevels")
 	var map_directory = DirAccess.get_directories_at(config.get_value("FileSystem", "InstallDir") + "/CustomWIPLevels")
 	print(map_directory)
-	
+	for i in map_directory:
+		print(config.get_value("FileSystem", "InstallDir") + "/CustomWIPLevels/" + i + "/Info.dat")
+		if !FileAccess.file_exists(config.get_value("FileSystem", "InstallDir") + "/CustomWIPLevels/" + i + "/Info.dat"):
+			print("Not Found")
+		else:
+			var infodat = FileAccess.open(config.get_value("FileSystem", "InstallDir") + "/CustomWIPLevels/" + i + "/Info.dat", FileAccess.READ)
+			var data = JSON.parse_string(infodat.get_as_text())
+			var map_button = MM_MAP.instantiate()
+			map_button.Title = data["_songName"]
+			map_button.Subtitle = data["_songSubName"]
+			map_button.Author = data["_songAuthorName"]
+			map_button.Mapper = data["_levelAuthorName"]
+			map_button.map_directory = config.get_value("FileSystem", "InstallDir") + "/CustomWIPLevels/" + i + "/"
+			map_button.Map_Image = data["_coverImageFilename"]
+			$ScrollContainer/VBoxContainer.add_child(map_button)
+			
+			
+
