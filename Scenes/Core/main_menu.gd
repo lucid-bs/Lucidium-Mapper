46c5aab2cc4f5b5c38c065a15da6ef737a4c2d49
JD Williams <jdwill239@gmail.com>
2023-11-09 13:50:05 -0500

    Add Map Configurer (temp)

--- Scenes/Core/main_menu.gd
+++ Scenes/Core/main_menu.gd
@@ -4,6 +4,7 @@ var config = ConfigFile.new()
 
 const MM_MAP = preload("res://Scenes/Core/Assets/mm_Map.tscn")
 
+const MAP_CONFIGURER = preload("res://Scenes/Core/map_configurer.tscn")
 
 func _ready() -> void:
 	
@@ -28,7 +29,10 @@ func _ready() -> void:
 			map_button.Mapper = data["_levelAuthorName"]
 			map_button.map_directory = config.get_value("FileSystem", "InstallDir") + "/CustomWIPLevels/" + i + "/"
 			map_button.Map_Image = data["_coverImageFilename"]
+			map_button.Map_Selected.connect(_configure_map)
 			$ScrollContainer/VBoxContainer.add_child(map_button)
 			
 			
 
+func _configure_map(directory: String):
+	get_tree().change_scene_to_packed(MAP_CONFIGURER)
