e6028d83695d234059d29d247cb27cd082ff81e5
JD Williams <jdwill239@gmail.com>
2023-11-15 20:24:57 -0500

    Get started on configuring map data

---
+++ Scenes/Core/Assets/MapDataManager.gd
@@ -0,0 +1,5 @@
+## Node for Managing and Updating Map Data (Info.dat).
+## Cannonically known as MapDataer.
+## Not known as this in editor for self-debugging and readability's sake.
+class_name MapDataManager extends Node 
+
---
+++ Scenes/Core/map_configurer.gd
@@ -0,0 +1,4 @@
+extends Control
+
+@export var MapDataer : MapDataManager 
+
