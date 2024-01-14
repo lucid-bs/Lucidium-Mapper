0ca9070c1fe2c7d2ec8bd9b133b7a79ec79fc231
JD Williams <jdwill239@gmail.com>
2024-01-13 22:25:38 -0500

    Adding splitting into UI

--- Scenes/Core/editor/selection_manager.gd
+++ Scenes/Core/editor/selection_manager.gd
@@ -3,4 +3,9 @@ extends Node
 ### Manages the selection of objects
 
 
-@export var selected_objects : Array
+@export var selected_objects : Array[VisualEventBS]
+
+signal object_selected(object: VisualEventBS)
+
+func update_property():
+	pass
