1a47ea5c5b44ca4c45e1bc7af4c28867a7bb6c43
JD Williams <jdwill239@gmail.com>
2024-01-27 15:20:42 -0500

    Fix weirdness

--- Scenes/Core/map_configurer.gd
+++ Scenes/Core/map_configurer.gd
@@ -19,7 +19,7 @@ func load_data():
 	$InfoDatThingy/MarginContainer/VBoxContainer/PanelContainer6/HBoxContainer/CoverFilenameEdit.text = map_data_manager.get_property(&"cover_image_filename")
 	$InfoDatThingy/MarginContainer/VBoxContainer/PanelContainer5/HBoxContainer/SongFilenameEdit.text = map_data_manager.get_property(&"song_filename")
 	$InfoDatThingy/MarginContainer/VBoxContainer/PanelContainer7/HBoxContainer/BPMSpinBox.value = map_data_manager.get_property(&"beats_per_minute")
-	$InfoDatThingy/MarginContainer/VBoxContainer/PanelContainer8/HBoxContainer2/PrevviewStartContainer/PreviewStartSpinBox.value = map_data_manager.get_property(&"preview_start")
-	$InfoDatThingy/MarginContainer/VBoxContainer/PanelContainer8/HBoxContainer2/PreviewDurationContainer/HBoxContainer/PreviewDurationSpinBox.value = map_data_manager.get_property(&"preview_duration")
+	$InfoDatThingy/MarginContainer/VBoxContainer/PanelContainer8/HBoxContainer2/PreviewStartContainer/PreviewStartSpinBox.value = map_data_manager.get_property(&"preview_start")
+	$InfoDatThingy/MarginContainer/VBoxContainer/PanelContainer8/HBoxContainer2/PreviewDurationContainer/PreviewDurationSpinBox.value = map_data_manager.get_property(&"preview_duration")
 func _on_song_name_edit_text_changed(new_text: String) -> void:
 	$MapDataManager.update_property(&"song_name", new_text)
