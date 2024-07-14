c7a78aca3cbc47fbaa6e4d64b1e719ac4acac810
JD Williams <jd@void-team.com>
2024-07-13 21:18:25 -0400

    Proper Timeline Scrolling

--- scenes/core/editor/beat_grid_bars.gd
+++ scenes/core/editor/beat_grid_bars.gd
@@ -3,4 +3,22 @@ extends Node3D
 @export var editor_node : LucidiumEditor
 
 func _ready() -> void:
-	pass
+	editor_node.beat_changed.connect(update_beat_bars)
+
+func update_beat_bars(new_beat : float):
+	var ceil_beat = ceil(new_beat)
+	var z_offset = ceil_beat - new_beat 
+	position.z = z_offset * -4 
+	
+	
+	$"0/BeatNum".text = str(ceil_beat - 1)
+	$"1/BeatNum".text = str(ceil_beat)
+	$"2/BeatNum".text = str(ceil_beat + 1)
+	$"3/BeatNum".text = str(ceil_beat + 2)
+	$"4/BeatNum".text = str(ceil_beat + 3)
+	$"5/BeatNum".text = str(ceil_beat + 4)
+	$"6/BeatNum".text = str(ceil_beat + 5)
+	$"7/BeatNum".text = str(ceil_beat + 6)
+	$"8/BeatNum".text = str(ceil_beat + 7)
+		
+	
