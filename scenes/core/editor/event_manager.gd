889bff645bc7f471938dc3ff9452ea2ce2bca166
JD Williams <jd@void-team.com>
2024-07-15 00:29:35 -0400

    Timeline Bar and tween Optimizations

--- scenes/core/editor/event_manager.gd
+++ scenes/core/editor/event_manager.gd
@@ -11,22 +11,21 @@ const BLOCK = preload("res://scenes/core/editor/block.tscn")
 
 @export var bpm_second_rate : float
 
-var tween : Tween
-
 var current_active_blocks : Array[ColorNote]
 func scroll_map(up: bool):
 	var step = 1.0 / editor_node.current_precision_denominator
 	if up:
 		editor_node.current_beat += step
-		editor_node.current_beat = clampf(editor_node.current_beat, 0.0, 1000.0)
+		editor_node.current_beat = clampf(editor_node.current_beat, 0.0, ($"../..".current_bpm / 60) * $"../..".audio_stream.get_length())
 	else:
 		editor_node.current_beat -= step
-		editor_node.current_beat = clampf(editor_node.current_beat, 0.0, 1000.0)
+		editor_node.current_beat = clampf(editor_node.current_beat, 0.0, ($"../..".current_bpm / 60) * $"../..".audio_stream.get_length())
 	sync_blocks()
 	
 	
 	
 func sync_blocks():
+	var block_tween = get_tree().create_tween().set_trans(Tween.TRANS_QUAD).set_parallel()
 	var notes = map_data.get_in_range(&"color_notes", editor_node.current_beat - 2, editor_node.current_beat + 7)
 	
 	var old_blocks : Array[Node] = $"../../Bloqs".get_children()
@@ -35,17 +34,15 @@ func sync_blocks():
 		if temp is Block:
 			temp.position.z = (i.beat - editor_node.current_beat) * -4
 			if temp.beat < editor_node.current_beat:
-				tween = create_tween().set_trans(Tween.TRANS_QUAD).set_parallel()
-				tween.tween_property(temp, "block_dissolve", 0.5, 0.1)
+				block_tween.tween_property(temp, "block_dissolve", 0.5, 0.1)
 				#temp.block_dissolve = 0.5
-				tween.tween_property(temp, "arrow_dissolve", 0.5, 0.1)
+				block_tween.tween_property(temp, "arrow_dissolve", 0.5, 0.1)
 				#temp.arrow_dissolve = 0.5
 			else:
-				tween = create_tween().set_trans(Tween.TRANS_QUAD).set_parallel()
 				#temp.block_dissolve = 1
-				tween.tween_property(temp, "block_dissolve", 1, 0.1)
+				block_tween.tween_property(temp, "block_dissolve", 1, 0.1)
 				#temp.arrow_dissolve = 1
-				tween.tween_property(temp, "arrow_dissolve", 1, 0.1)
+				block_tween.tween_property(temp, "arrow_dissolve", 1, 0.1)
 			old_blocks.erase(temp)
 			
 		else:
@@ -62,22 +59,21 @@ func sync_blocks():
 			$"../../Bloqs".add_child(new_block)
 			new_block.position.z = (i.beat - editor_node.current_beat) * -4
 			if new_block.beat < editor_node.current_beat:
-				tween = create_tween().set_trans(Tween.TRANS_QUAD).set_parallel()
-				tween.tween_property(new_block, "block_dissolve", 0.5, 0.1)
+				block_tween.tween_property(new_block, "block_dissolve", 0.5, 0.1)
 				#temp.block_dissolve = 0.5
-				tween.tween_property(new_block, "arrow_dissolve", 0.5, 0.1)
+				block_tween.tween_property(new_block, "arrow_dissolve", 0.5, 0.1)
 				#temp.arrow_dissolve = 0.5
 			else:
-				tween = create_tween().set_trans(Tween.TRANS_QUAD).set_parallel()
 				#temp.block_dissolve = 1
-				tween.tween_property(new_block, "block_dissolve", 1, 0.1)
+				block_tween.tween_property(new_block, "block_dissolve", 1, 0.1)
 				#temp.arrow_dissolve = 1
-				tween.tween_property(new_block, "arrow_dissolve", 1, 0.1)
+				block_tween.tween_property(new_block, "arrow_dissolve", 1, 0.1)
 			i.set_meta(&"block_node", new_block)
 			
 		
 			
 	for i : Block in old_blocks:
 		i.color_note_resource.set_meta(&"block_node", null)
-		i.queue_free()
+		block_tween.tween_callback(i.queue_free)
+		
 		
--- scenes/core/editor/player_controller.gd
+++ scenes/core/editor/player_controller.gd
@@ -8,18 +8,22 @@ signal player_scrolled(up: bool)
 @export var event_manager : EventManager
 @export var editor_node : LucidiumEditor
 
+@export var timeline_scroll_bar : HScrollBar
+
+
+
 var playback_node : MapPlayback
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("map_scroll_up") && event.is_pressed() && !editor_node.map_playing:
 		event_manager.scroll_map(true)
 		editor_node.audio_stream_player.play((60/$"../..".current_bpm) * editor_node.current_beat)
-		await get_tree().create_timer(0.13).timeout
+		await get_tree().create_timer(0.15).timeout
 		editor_node.audio_stream_player.stop()
 	elif event.is_action("map_scroll_down") && event.is_pressed() && !editor_node.map_playing:
 		event_manager.scroll_map(false)
 		editor_node.audio_stream_player.play((60/$"../..".current_bpm) * editor_node.current_beat)
-		await get_tree().create_timer(0.13).timeout
+		await get_tree().create_timer(0.15).timeout
 		editor_node.audio_stream_player.stop()
 		
 		
@@ -31,13 +35,36 @@ func _input(event: InputEvent) -> void:
 			playback_node.queue_free()
 			editor_node.current_beat = snappedf(editor_node.current_beat, (1.0/divisor))
 			event_manager.sync_blocks()
+			timeline_scroll_bar.value_changed.connect(scroll_bar_value_changed)
+			
 		else:
+			editor_node.map_playing = true
+			if editor_node.audio_stream_player.playing:
+				await get_tree().create_timer(0.15).timeout
+				editor_node.audio_stream_player.stop()
 			editor_node.audio_stream_player.stream =  $"../..".audio_stream
 			editor_node.audio_stream_player.play((60/$"../..".current_bpm) * editor_node.current_beat)
-			editor_node.map_playing = true
+			
 			
 			playback_node = MapPlayback.new()
 			playback_node.editor_node = editor_node
 			playback_node.event_manager = event_manager
 			editor_node.add_child(playback_node)
+			timeline_scroll_bar.value_changed.disconnect(scroll_bar_value_changed)
 			
+			
+func _ready() -> void:
+	timeline_scroll_bar.step = 1 / editor_node.current_precision_denominator
+	timeline_scroll_bar.max_value = ($"../..".current_bpm / 60) * $"../..".audio_stream.get_length()
+	timeline_scroll_bar.value_changed.connect(scroll_bar_value_changed)
+	editor_node.beat_changed.connect(editor_beat_changed)
+
+func scroll_bar_value_changed(value):
+	editor_node.current_beat = value
+	event_manager.sync_blocks()
+	editor_node.audio_stream_player.play((60/$"../..".current_bpm) * editor_node.current_beat)
+	await get_tree().create_timer(0.13).timeout
+	editor_node.audio_stream_player.stop()
+	
+func editor_beat_changed(value):
+	timeline_scroll_bar.value = value
