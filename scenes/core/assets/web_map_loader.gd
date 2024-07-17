9706a8f9c85f76da4f16f5a363c66556cb6f0224
JD Williams <jd@void-team.com>
2024-07-17 14:37:42 -0400

    no clue how *that* got disconnected

--- scenes/core/assets/web_map_loader.gd
+++ scenes/core/assets/web_map_loader.gd
@@ -55,6 +55,10 @@ func _on_button_pressed() -> void:
 		print_debug("ERROR - FILE INVALID")
 	else:
 		print_debug("FILE VALID")
-		var info = JSON.parse_string(reader.read_file("Info.dat").get_string_from_ascii())
+		var info = JSON.parse_string(reader.read_file("Info.dat", false).get_string_from_ascii())
 		print(info)
-		
+		var root_dir = config.get_value("FileSystem", "InstallDir") if $FILESYSTEM/TabBar.current_tab == 0 else "user://"
+		print(root_dir)
+		var root_dir_access = DirAccess.open(root_dir)
+		print(root_dir_access.make_dir_recursive("CustomWIPLevels/" + info["_songName"]))
+		print(info["_songName"])
