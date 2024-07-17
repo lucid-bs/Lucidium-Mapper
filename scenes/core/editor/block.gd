7d297ad26b0a2837a4f179c3e641ed30295193de
JD Williams <jd@void-team.com>
2024-07-17 14:05:34 -0400

    Adjust Blocks

--- scenes/core/editor/block.gd
+++ scenes/core/editor/block.gd
@@ -33,9 +33,9 @@ enum BLOQ_COLORS {
 # END OPF V3 JSON SETTINGS
 
 @export var rgb_color : Color
-@export var bloq_multiplier : float = 1 # 0.28 MMA2 Style 0.4 CM Style
-@export var arrow_multiplier : float = 0.8 # 0.405 MMA2 Style 1.3 CM Style
-@export var arrow_white : float = 0 # 0 MMA2 Style 0.125 CM Style
+@export var bloq_multiplier : float = 2 # 0.28 MMA2 Style 0.4 CM Style 2 LUC Style
+@export var arrow_multiplier : float = 8 # 0.405 MMA2 Style 1.3 CM Style 8 LUC Style
+@export var arrow_white : float = 0 # 0 MMA2 Style 0.125 CM Style 0 LUC Style
 
 @export var block_dissolve : float = 1:
 	set(value):
