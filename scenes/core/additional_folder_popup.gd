ce5048a4d127e32cfc57c5d7fc997f87ca651953
JD Williams <jd@void-team.com>
2025-01-28 09:09:29 -0500

    Un-Screwup the Scripts

--- scenes/core/additional_folder_popup.gd
+++ scenes/core/additional_folder_popup.gd
@@ -4,16 +4,23 @@ extends Window
 func _on_close_requested() -> void:
 	hide()
 
-
+func _on_visibility_changed() -> void:
+	if visible:
+		$Panel/MarginContainer/VBoxContainer/HBoxContainer2/Confirm.disabled = true
 
 func _on_back_pressed() -> void:
 	hide()
 
 
+func _on_line_edit_text_changed(new_text: String) -> void:
+	if new_text != "":
+		$Panel/MarginContainer/VBoxContainer/HBoxContainer2/Confirm.disabled = false
+	else:
+		$Panel/MarginContainer/VBoxContainer/HBoxContainer2/Confirm.disabled = true
+
 
 func _on_confirm_pressed() -> void:
 	hide()
-
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	$Panel/MarginContainer/VBoxContainer/HBoxContainer2/Confirm.pressed.emit()
